30fee2a6731d68dc2c8f7492d463ba0602c25019
alinakh1 <akhabibullina@nyc.yearup.org>
2022-10-24 13:26:53 -0400

    first commit

---
+++ Functions/simple_functions.js
@@ -0,0 +1,24 @@
+
+
+function favoriteThings() {
+    let name = "Alina";
+    console.log("my name is " + name);
+    let favoriteMovie = "Annabel";
+    console.log("and my favorite movie is " + favoriteMovie);
+    let favoriteBand = "Imagine Dragons";
+    console.log("my favorite band is " + favoriteBand);
+    
+}
+
+favoriteThings();
+
+
+function whyImHere() {
+    console.log("I joined Learn To Code Program to become a full stack developer");
+}
+whyImHere();
+
+function favoritePlace(){
+    console.log("My favorite place is Stockholm because I like European architecture ")
+}
+favoritePlace();
