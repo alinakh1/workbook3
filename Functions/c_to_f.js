d96aca9ae24a769385641aecca6c0a8c26a36b78
alinakh1 <akhabibullina@nyc.yearup.org>
2022-10-25 11:09:57 -0400

    commit

---
+++ Functions/c_to_f.js
@@ -0,0 +1,64 @@
+function convertFtoC(celsiusTemp)
+{
+    console.log("Temperature in celsius " + celsiusTemp)
+
+}
+
+const a =1.8;
+const b = 32;
+let currentTemp = 92;
+let celsiusTemp = (currentTemp * a) + b;
+
+convertFtoC(celsiusTemp);
+
+
+/*const numberField2 = document.getElementById("numberField2");
+    const numA = 5/9;
+    const numB = 32;
+
+    let num2AsString = numberField2.value;
+
+
+    let num2AsNumber = Number(num2AsString);
+
+    let answer = (num2AsNumber - numB) * numA ;*/
+
+
+
+function convertFtoC(celsiusTemp, fahrenheitTemp)
+{
+    
+    console.log("Temperature in celsius " + celsiusTemp)
+    console.log("Temperature in fahrenheit " + fahrenheitTemp)
+
+}
+
+/*const a =5/9;
+const b = 32;
+let currentTempFahr = 92;
+let currentTempCels = 33;
+const c = 1.8;
+let celsiusTemp = (currentTempFahr - b) * a;
+let fahrenheitTemp = (currentTempCels * c) + b;
+
+convertFtoC(celsiusTemp, fahrenheitTemp);*/
+
+//This is an example, everything that I didn't use but I actually programmed two converters in one javascript
+
+
+federalTaxWithhold = (cels * c) + d;
+
+//here I will be counting the federal tax based on the withholding code
+
+/*    
+function getFederalTax()
+    {
+        console.log( "The Federal tax is " + taxFed );
+    }
+    let payGross = 20000;
+    let federalTax = 1.45 /100;
+    let taxFed = medicareTax * payGross ;
+    
+    getMedicareTax(payGross, medicareTax, taxMed )
+
+ */
